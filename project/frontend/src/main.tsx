d7c82286444882b662f3213a8ab03d95fefbfb4d
Niklas <niklasberby@gmail.com>
2024-10-23 18:19:24 +0200

    init

--- project/frontend/src/main.tsx
+++ project/frontend/src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { render } from 'preact'
+import { App } from './app.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+render(<App />, document.getElementById('app')!)
